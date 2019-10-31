4aacef842ed339e158dc7617818f8bf0f7e50740
Dom <domeccleston@gmail.com>
2019-10-31 17:45:03 +0000

    get actions for a project

--- projectsRouter.js
+++ projectsRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const db = require('./data/helpers/projectModel');
+const projectsDb = require('./data/helpers/projectModel');
+
 const router = express.Router();
 
 // error handling middleware
 
 function isValidParamId(req, res, next) {
-  console.log(req.params.id);
   // eslint-disable-next-line radix
   if (parseInt(req.params.id) > 0) {
     next();
@@ -17,7 +17,8 @@ function isValidParamId(req, res, next) {
 // routes for /api/projects/
 
 router.get('/', (req, res) => {
-  db.get()
+  projectsDb
+    .get()
     .then(projects => {
       res.status(200).json(projects);
     })
@@ -27,7 +28,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', isValidParamId, (req, res) => {
-  db.get(req.params.id)
+  projectsDb
+    .get(req.params.id)
     .then(project => {
       if (project) {
         res.status(200).json(project);
@@ -40,8 +42,24 @@ router.get('/:id', isValidParamId, (req, res) => {
     });
 });
 
+router.get('/:id/actions', isValidParamId, (req, res) => {
+  projectsDb
+    .getProjectActions(req.params.id)
+    .then(projectActions => {
+      if (projectActions.length > 1) {
+        res.status(200).json(projectActions);
+      } else {
+        res.status(404).json('No projects found for that user');
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 router.put('/:id', isValidParamId, (req, res) => {
-  db.update(req.params.id, req.body)
+  projectsDb
+    .update(req.params.id, req.body)
     .then(project => {
       if (project) {
         res.status(200).json(project);
@@ -55,10 +73,11 @@ router.put('/:id', isValidParamId, (req, res) => {
 });
 
 router.delete('/:id', isValidParamId, (req, res) => {
-  db.remove(req.params.id)
+  projectsDb
+    .remove(req.params.id)
     .then(project => {
       if (project) {
-        res.status(200).json(project);
+        res.status(200).json('Project deleted succesfully.');
       } else {
         res.status(404).json('Invalid project ID');
       }
@@ -69,7 +88,8 @@ router.delete('/:id', isValidParamId, (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  db.insert(req.body)
+  projectsDb
+    .insert(req.body)
     .then(project => {
       res.status(200).json({ message: 'New project created successfully', project });
     })
